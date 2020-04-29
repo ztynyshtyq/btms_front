ee5aa934bb553711154a5d8c409c6b4f349b2faf
Maxim Tsoy <maxim.tsoy.cv@gmail.com>
2020-04-29 23:45:22 +0600

    Changelog 0.9.1.4 - added reject and approve requests

---
+++ src/project/expense_reports/requests/send_reject.jsx
@@ -0,0 +1,24 @@
+import * as urls from "../constants/urls";
+import * as events from "../../../core/auth/events";
+import * as core_events from "../../../core/events";
+import setSingleExpenseReportFromUserRequests from "../actions/set_single_expense_report_for_user.action";
+import {toast} from 'react-toastify';
+
+export const apiSendOnConfirmation = (accessToken, expenseReport) => dispatch => {
+    dispatch(core_events.eventInitRequest());
+    return fetch(urls.API_REJECT, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": accessToken
+        },
+        body: "report_id=" + expenseReport.id,
+    }).then(response => response.json()/*dispatch(events.eventAuthFailed())*/)
+        .then(json => {
+            dispatch(setSingleExpenseReportFromUserRequests({...expenseReport, status: "initial"}))
+            toast("You have rejected this report", {
+                autoClose: 5000,
+                className: "uc_toast"
+            });
+        }, error => dispatch(events.eventAuthFailed()));
+}
